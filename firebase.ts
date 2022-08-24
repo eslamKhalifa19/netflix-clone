c74e702137c80a58831954534f76159bb12ae39e
Eslam Khalefa <eslamkhalefa19@gmail.com>
2022-08-24 10:32:23 +0200

    firebase_config

---
+++ firebase.ts
@@ -0,0 +1,19 @@
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getApps, getApp, initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.Next_FIREBASE_API_KEY,
+  authDomain: "netflix-37210.firebaseapp.com",
+  projectId: "netflix-37210",
+  storageBucket: "netflix-37210.appspot.com",
+  messagingSenderId: "908931748232",
+  appId: "1:908931748232:web:75bbe69584857bb6a82a3d"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp()
+const db = getFirestore()
+const auth = getAuth()
+
+export default app
+export { auth, db }
