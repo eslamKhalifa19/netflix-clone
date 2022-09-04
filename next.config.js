8836f789d720989b8c2efff8a7348c2b209a49ec
Eslam Khalefa <eslamkhalefa19@gmail.com>
2022-09-04 12:48:50 +0200

    fix_firebase_api_key

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   env: {
-    API_KEY: process.env.API_KEY,
+    Next_API_KEY: process.env.Next_API_KEY,
     NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
   },
-
   reactStrictMode: true,
   images: {
     domains: ["image.tmdb.org", "rb.gy"],
