88eca2a5a6e721cb29f731d1254d2f22394799ac
Eslam Khalefa <eslamkhalefa19@gmail.com>
2022-11-22 02:44:19 +0200

    add_film_details_in_modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -91,6 +91,38 @@ function Modal() {
             </button>
           </div>
         </div>
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie!.vote_average * 10}% Match
+              </p>
+              <p className="font-light">
+                {movie?.release_date || movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs">
+                HD
+              </div>
+            </div>
+            <div className="flex flex-col gap-x-10 gay-y-4 font-light md:flex-row">
+              <p className="w-5/6">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres:</span>
+                  {genres.map((genre) => genre.name).join(", ")}
+                </div>
+                <div>
+                  <span className="text-[gray]">Original language:</span>
+                  {movie?.original_language}
+                </div>
+                <div>
+                  <span className="text-[gray] ">Total votes:</span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </>
     </MuiModal>
   );
