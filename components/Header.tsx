b8ffad04ea5e408bfae0c8896e4d3ccb9260f1c8
Eslam Khalefa <eslamkhalefa19@gmail.com>
2022-11-23 04:18:04 +0200

    add_menu_in_header_In_small_screens

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,6 +2,7 @@ import { BellIcon, SearchIcon } from "@heroicons/react/solid";
 import { useEffect, useState } from "react";
 import Link from "next/link";
 import useAuth from "../hooks/useAuth";
+import BasicMenu from "./BasicMenu";
 
 function Header() {
   const [isScrolled, setIsScrolled] = useState(false);
@@ -31,6 +32,7 @@ function Header() {
           height={100}
           className="cursor-pointer object-contain"
         />
+        <BasicMenu />
 
         <ul className="hidden space-x-4 md:flex">
           <li className="headerLink ">Home</li>
@@ -44,14 +46,12 @@ function Header() {
         <SearchIcon className="sm hidden h-6 w-6 sm:inline" />
         <p className="hidden lg:inline">Kids</p>
         <BellIcon className="h-6 w-6" />
-        {/* <Link href="/account"> */}
         <img
           onClick={logout}
           src="https://rb.gy/g1pwyx"
           alt=""
           className="cursor-pointer rounded"
         />
-        {/* </Link> */}
       </div>
     </header>
   );
